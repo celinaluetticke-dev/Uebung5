acfafe16aa960bfcfb8a96715ca4de6da5c54355
celinaluetticke-dev <celina.luetticke@gmail.com>
2025-11-21 10:11:54 +0100

    Hausaufgabe 1

---
+++ src/h1/H1_main.java
@@ -0,0 +1,20 @@
+package h1;
+
+public class H1_main {
+	public static void main(String[] args) {
+
+		int[] myArray = {1, 2, 3, 4};
+
+		int[] myArrayreversed = new int[myArray.length];
+
+		int i = 0;
+		while (i < myArray.length) {
+			myArrayreversed[i] = myArray[myArray.length - 1 - i];
+			i = i + 1;
+		}
+
+		System.out.println("Eingabe: " + java.util.Arrays.toString(myArray));
+		System.out.println("Rueckwaerts: " + java.util.Arrays.toString(myArrayreversed));
+
+		}
+	}
